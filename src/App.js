ee715b017552cdad6c849c4879d664effa9ce995
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-12-15 17:31:53 -0500

    setup scene changing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 //AppJS will hold state and determine what gets rendered
 //Make sure to have ability to go back and forth scenes
 
+import { ScreenSpace } from '@react-three/drei';
+import { useState } from 'react';
 import './App.css';
 import SelectionScene from './scenes/SelectionScene/SelectionScene';
 
+const Scenes = {
+  IntroScene: 'IntroScene',
+  SelectionScene: 1,
+  MemoryCardSelectionScene: 2,
+  ObjectSelectionScene: 3
+}
+
 function App() {
+  const [currScene, setCurrScene] = useState(Scenes.IntroScene);
+
   return (
     <div className="App">
       <SelectionScene />
+      {(() => {
+        switch (currScene) {
+          case Scenes.IntroScene:
+          case Scenes.SelectionScene: <SelectionScene />
+          case Scenes.MemoryCardSelectionScene:
+          case ScreenSpace.ObjectSelectionScene:
+        }
+      })()}
     </div>
   );
 };
