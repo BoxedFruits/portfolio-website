c2b720b1688d51c62b89462b7a25867c3ff01038
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-12-15 21:25:32 -0500

    go to memeory card selection

--- src/App.js
+++ src/App.js
@@ -7,24 +7,29 @@ import './App.css';
 import SelectionScene from './scenes/SelectionScene/SelectionScene';
 
 const Scenes = {
-  IntroScene: 'IntroScene',
+  IntroScene: 0,
   SelectionScene: 1,
   MemoryCardSelectionScene: 2,
   ObjectSelectionScene: 3
 }
 
-function App() {
-  const [currScene, setCurrScene] = useState(Scenes.IntroScene);
+const App = () => {
+  const [currScene, setCurrScene] = useState(Scenes.SelectionScene);
 
   return (
     <div className="App">
-      <SelectionScene />
       {(() => {
         switch (currScene) {
           case Scenes.IntroScene:
-          case Scenes.SelectionScene: <SelectionScene />
-          case Scenes.MemoryCardSelectionScene:
+            break;
+          case Scenes.SelectionScene: 
+            return <SelectionScene nextScene={() => setCurrScene(Scenes.MemoryCardSelectionScene)}></SelectionScene>
+            break;
+          case Scenes.MemoryCardSelectionScene: return <h1>Memory Card selection</h1>
+            break;
           case ScreenSpace.ObjectSelectionScene:
+            break;
+          default: <h1>Uh oh someething broke</h1>
         }
       })()}
     </div>
--- src/scenes/SelectionScene/SelectionScene.jsx
+++ src/scenes/SelectionScene/SelectionScene.jsx
@@ -2,32 +2,36 @@ import './SelectionSceneStyles.css'
 import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Color } from 'three';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const SceneSetup = () => {
   const { scene } = useThree();
   scene.background = new Color('#000000');
 };
 
-const SelectionText = () => {
-  const [isHighlighted, setIsHighLighted] = useState(undefined);
-  // why am i blanking out on a better way to do this. Can probably use enums
+const SelectionScene = ({nextScene, anotherProp}) => {
+  console.log(anotherProp)
+  
+console.log(nextScene)
+  const SelectionText = () => {
+    const [isHighlighted, setIsHighLighted] = useState(undefined);
+    // why am i blanking out on a better way to do this. Can probably use enums
+  
+    return (
+      <div style={{ position: 'absolute', zIndex: '1' }}>
+        <button className={`selection-browser ${isHighlighted ? 'highlight' : ''}`} 
+          onMouseEnter={(e) => setIsHighLighted(true)}
+          onClick={()=> {nextScene()}}
+          >
+          Browser
+        </button>
+        <button className={`selection-config' ${!isHighlighted && isHighlighted !== undefined ? 'highlight' : ''}`} onMouseEnter={(e) => setIsHighLighted(false)}>
+          System Configuration
+        </button>
+      </div>
+    );
+  }
 
-  return (
-    <div style={{ position: 'absolute', zIndex: '1' }}>
-      <button className={`selection-browser ${isHighlighted ? 'highlight' : ''}`} onMouseEnter={(e) => setIsHighLighted(true)}>
-        Browser
-      </button>
-      <button className={`selection-config' ${!isHighlighted && isHighlighted !== undefined ? 'highlight' : ''}`} onMouseEnter={(e) => setIsHighLighted(false)}>
-        System Configuration
-      </button>
-    </div>
-  )
-
-
-}
-
-const SelectionScene = ({ nextScene, prevScene }) => {
   return (
     <>
       <SelectionText />
