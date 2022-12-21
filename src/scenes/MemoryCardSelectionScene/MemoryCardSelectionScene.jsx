1abcf286a615cabdd26e07c1f1ec257d5603a524
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-12-21 17:11:03 -0500

    add json data to component

--- src/scenes/MemoryCardSelectionScene/MemoryCardSelectionScene.jsx
+++ src/scenes/MemoryCardSelectionScene/MemoryCardSelectionScene.jsx
@@ -1,41 +1,49 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
+const SideProjects = require("./MemoryCards/sideProjects.json");
+const WorkExperience = require("./MemoryCards/workExperience.json")
 
 // const SceneSetup = () => {
 //   const { scene } = useThree();
 // };
 
 const Highlight = { // Is this even worse?
-  DEFAULT: 0,
-  "Job Experience": 1,
-  "Side Projects": 2
+  "DEFAULT": '',
+  "Work Experience": "Work Experience",
+  "Side Projects": "Side Projects"
 }
 
 const MemoryCardSelectionScreen = () => {
   const [currHighlighted, setCurrHighLighted] = useState(Highlight.DEFAULT);
-
-  const MemoryCard = ({ position, model, color, name, highlight }) => {
-    console.log(name, currHighlighted)
+  const MemoryCard = ({ position, model, color, name, file }) => {
     return (
       <mesh
         position={position}
-        onPointerOver={(event) => { setCurrHighLighted(name); console.log(event, name) }}>
+        onPointerOver={() => setCurrHighLighted(name)}>
         <sphereGeometry></sphereGeometry>
-        <meshPhongMaterial color={highlight !== name ? color : 'pink'}></meshPhongMaterial>
+        <meshPhongMaterial color={currHighlighted !== name ? color : 'pink'}></meshPhongMaterial>
       </mesh>
     )
   }
 
   return (
     <>
-      <h1 style={{ zIndex: 1 }}>Memory Card selection2</h1>
+      <h1 style={{ zIndex: 1 }}>Memory Card selection2 / {currHighlighted}</h1>
       <Canvas>
         {/* <OrbitControls /> */}
         <ambientLight></ambientLight>
         <pointLight intensity={0.1}></pointLight>
-        <MemoryCard position={[-2.25, 0, 0]} name={Highlight["Job Experience"]} color="blue" highlight={currHighlighted} />
-        <MemoryCard position={[2.25, 0, 0]} name={Highlight["Side Projects"]} color="white" highlight={currHighlighted} />
+        <MemoryCard
+          position={[-2.25, 0, 0]}
+          name={Highlight["Work Experience"]}
+          color="blue"
+          file={WorkExperience}/>
+        <MemoryCard
+          position={[2.25, 0, 0]}
+          name={Highlight["Side Projects"]}
+          color="white"
+          file={SideProjects}/>
       </Canvas>
     </>
   );
