981a9d7dd246de458f273f6481f82f15b75388ce
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-12-22 21:17:34 -0500

    replace place holder with 3d model

--- src/scenes/MemoryCardSelectionScene/MemoryCardSelectionScene.jsx
+++ src/scenes/MemoryCardSelectionScene/MemoryCardSelectionScene.jsx
@@ -1,6 +1,10 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import { useLoader } from '@react-three/fiber'
+
 const SideProjects = require("./MemoryCards/sideProjects.json");
 const WorkExperience = require("./MemoryCards/workExperience.json")
 
@@ -14,37 +18,60 @@ const Highlight = { // Is this even worse?
   "Side Projects": "Side Projects"
 }
 
+const ObjectSelection = () => {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry></boxGeometry>
+      </mesh>
+    </Canvas>
+  )
+}
+
 const MemoryCardSelectionScreen = () => {
   const [currHighlighted, setCurrHighLighted] = useState(Highlight.DEFAULT);
-  const MemoryCard = ({ position, model, color, name, file }) => {
+  const [viewObjects, setViewObjects] = useState();
+
+  const MemoryCard = ({ position, model, color, name, file }) => { // Pass in setCurrHighLighted and currhighlighted as props and then make this a sperate component
+    const { scene } = useLoader(GLTFLoader, 'memory_card.glb')
+    const copiedScene = useMemo(() => scene.clone(), [scene])
+
     return (
-      <mesh
-        position={position}
-        onPointerOver={() => setCurrHighLighted(name)}>
-        <sphereGeometry></sphereGeometry>
-        <meshPhongMaterial color={currHighlighted !== name ? color : 'pink'}></meshPhongMaterial>
-      </mesh>
-    )
+      <group>
+        <primitive
+          onPointerOver={() => setCurrHighLighted(name)} 
+          position={position}
+          onClick={() => {setViewObjects(true);}}
+          object={copiedScene}>
+        </primitive>
+      </group>
+    );
   }
 
   return (
     <>
       <h1 style={{ zIndex: 1 }}>Memory Card selection2 / {currHighlighted}</h1>
-      <Canvas>
-        {/* <OrbitControls /> */}
-        <ambientLight></ambientLight>
-        <pointLight intensity={0.1}></pointLight>
-        <MemoryCard
-          position={[-2.25, 0, 0]}
-          name={Highlight["Work Experience"]}
-          color="blue"
-          file={WorkExperience}/>
-        <MemoryCard
-          position={[2.25, 0, 0]}
-          name={Highlight["Side Projects"]}
-          color="white"
-          file={SideProjects}/>
-      </Canvas>
+      {viewObjects ?
+        <ObjectSelection>
+
+        </ObjectSelection>
+        :
+        <Canvas>
+          <OrbitControls />
+          <ambientLight></ambientLight>
+          <pointLight intensity={0.1}></pointLight>
+          <MemoryCard
+            position={[-2.25, 0, 0]}
+            name={Highlight["Work Experience"]}
+            color="blue"
+            file={WorkExperience} />
+          <MemoryCard
+            position={[2.25, 0, 0]}
+            name={Highlight["Side Projects"]}
+            color="white"
+            file={SideProjects} />
+        </Canvas>}
+
     </>
   );
 }
