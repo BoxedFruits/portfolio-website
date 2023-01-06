c237c9d8173789f3997b9619d271d1ac382fb064
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-01-06 13:43:11 -0500

    go back to sprite for glow orbs

--- src/components/GlowOrbs/GlowOrbs.jsx
+++ src/components/GlowOrbs/GlowOrbs.jsx
@@ -1,16 +1,45 @@
 
-const GlowOrbs = (props) => {
-  return (
-    <mesh position={[0, 0, 0]} {...props} scale={.6}>
-      <sphereGeometry />
-      <meshPhongMaterial
-        color='white'
-        emissive='white'
-        emissiveIntensity={1.25}
-        toneMapped={false}
-      />
-    </mesh>
-  )
-}
+import { useTexture } from '@react-three/drei';
+import { AdditiveBlending } from 'three';
 
-export default GlowOrbs;
+const GlowOrbs = () => {
+  const orbTexture = useTexture("glow.png")
+  return (
+    <>
+      {/* <mesh scale={1} position={[0,0,1]}>
+        <planeGeometry  args={[5, 5]}></planeGeometry>
+        <meshBasicMaterial
+          map={orbTexture} {...config}
+          alphaMap={orbTexture}
+        // color="red"
+        >
+        </meshBasicMaterial>
+      </mesh> */}
+
+      <sprite scale={3} position={[0, 0, .6]}>
+        <spriteMaterial // should grow and become smaller
+          map={orbTexture}
+          alphaMap={orbTexture}
+          transparent={true}
+          opacity={1}
+          blending={AdditiveBlending}
+          depthWrite={false}
+          toneMapped={false}
+        />
+      </sprite>
+      <mesh scale={.36} position={[0, 0, .6]}>
+        <sphereGeometry />
+        <meshPhongMaterial
+          color='white'
+          emissive='white'
+          emissiveIntensity={40.25}
+          toneMapped={false}
+          transparent={true}
+          opacity={0}
+        />
+      </mesh>
+
+    </>
+  );
+}
+export default GlowOrbs
