eec164b895ef4838719f6f781b220f497daba0d1
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-01-09 17:15:19 -0500

    animate vanguard logo loading in

--- src/scenes/MemoryCardSelectionScene/ObjectSelector/ObjectSelector.jsx
+++ src/scenes/MemoryCardSelectionScene/ObjectSelector/ObjectSelector.jsx
@@ -5,7 +5,7 @@
 
 import { ArcballControls, Html, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import GlowOrbs from "../../../components/GlowOrbs/GlowOrbs";
 import Modal from "../Modal/Modal";
 import { VanguardLogo } from "../TitleModels/VanguardLogo";
@@ -14,7 +14,7 @@ const OBJECTS_IN_ROW = 5;
 
 //TODO: Refactor this to be more flexible. Won't be able to use this for the spinning object in the Modal
 //Might be able to combine these two functions with optional parameters and destructuring 
-const getModelForSelection = (title, position, index, onHandleAnimation, setOrbPostion, setCurrHighLighted) => {
+const getModelForSelection = (title, position, index, onHandleAnimation, setOrbPostion, setCurrHighLighted, getRef, animateNextObject) => {
   switch (title) {
     case "Vanguard":
       return <VanguardLogo
@@ -24,8 +24,10 @@ const getModelForSelection = (title, position, index, onHandleAnimation, setOrbP
         onPointerOver={() => {
           setOrbPostion();
           setCurrHighLighted();
-        }
-        }
+        }}
+        getRef={getRef}
+        scale={0}
+        animationCallback={() => animateNextObject()} //this is what will move the count to the next object to animate
       />;
     default: return <Text>uh oh something broke</Text>
   }
@@ -45,11 +47,12 @@ const getModelForModal = (title, index) => {
 }
 
 const ObjectSelector = ({ jsonObject, memoryCardName }) => {
-  let zValue = 3.25;
-
   const [animateBackground, setAnimateBackground] = useState(false);
   const [orbPostion, setOrbPostion] = useState([-5, -0.5, -.50]);
   const [currHighlighted, setCurrHighLighted] = useState(jsonObject.objects[0].title)
+  const [objectsToRender, setObjectsToRender] = useState([]);
+  const [animatedObjectsCounter, setAnimatedObjectsCounter] = useState(0); // Keeps track of which object to animate
+  const [objectRefs, setObjectRefs] = useState([])
 
   const objIndex = useRef(null);
 
@@ -57,6 +60,42 @@ const ObjectSelector = ({ jsonObject, memoryCardName }) => {
     objIndex.current = index;
     setAnimateBackground(true);
   }
+
+  useEffect(() => {
+    let zValue = 3.25;
+    const objects = jsonObject.objects.map((obj, index) => {
+      if (index % OBJECTS_IN_ROW === 0) zValue -= 3;
+      let position = [-5 + (index % OBJECTS_IN_ROW * 2.5), 0, zValue];
+
+      return getModelForSelection(
+        obj.model,
+        position,
+        index,
+        () => handleAnimation(index),
+        () => setOrbPostion([position[0], position[1] - 0.45, position[2] - 0.75]),
+        () => setCurrHighLighted(obj.title),
+        (e) => setObjectRefs(objectRefs => ([...objectRefs, e])),
+        () => {console.log("animationCallback");setAnimatedObjectsCounter(index + 1)}
+      )
+    })
+    setObjectsToRender(objects);
+  }, [])
+
+  useEffect(() => {
+    if (objectRefs[0]?.current) {
+      console.log(objectRefs[0].current)
+      objectRefs[0].current.startLoadingAnimation()
+    }
+  }, [objectRefs])
+
+  useEffect(() => {
+    console.log("animated objects Counter ", animatedObjectsCounter);
+    if (objectRefs[animatedObjectsCounter]?.current) {
+      objectRefs[animatedObjectsCounter].current.startLoadingAnimation()
+    } else {
+      console.log("tried to access index out of bounds?")
+    }
+  }, [animatedObjectsCounter])
 
   return (
     <>
@@ -76,26 +115,13 @@ const ObjectSelector = ({ jsonObject, memoryCardName }) => {
             Memory Card
             <span style={{ fontSize: "24px" }}> (PS2) &nbsp;/</span>
             &nbsp;{memoryCardName}
-            <p style={{marginTop: "0px", fontSize: "24px"}}>46,341 KB FREE</p>
+            <p style={{ marginTop: "0px", fontSize: "24px" }}>46,341 KB FREE</p>
           </p>
           <h1 className="text-shadow" style={{ float: 'right', marginRight: '24px', color: 'rgb(221, 221, 78)', fontFamily: "arial", fontWeight: "lighter" }}>{currHighlighted}</h1>
         </Html>
         <ambientLight />
         <GlowOrbs position={orbPostion} />
-        {
-          jsonObject.objects.map((obj, index) => {
-            if (index % OBJECTS_IN_ROW === 0) zValue -= 3;
-            let position = [-5 + (index % OBJECTS_IN_ROW * 2.5), 0, zValue];
-            return getModelForSelection(
-              obj.model,
-              position,
-              index,
-              () => handleAnimation(index),
-              () => setOrbPostion([position[0], position[1] - 0.45, position[2] - 0.75]),
-              () => setCurrHighLighted(obj.title)
-            )
-          })
-        }
+        {objectsToRender}
       </Canvas>
     </>
   )
