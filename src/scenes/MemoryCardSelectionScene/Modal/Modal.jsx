e4b7cf6c651d7dd7de398204a538dee8b7d1f07e
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-01-19 17:49:14 -0500

    change scroll bar and update a little css

--- src/scenes/MemoryCardSelectionScene/Modal/Modal.jsx
+++ src/scenes/MemoryCardSelectionScene/Modal/Modal.jsx
@@ -41,39 +41,37 @@ const Modal = ({ data, memoryCardName, closeModal, shrinkModel }) => {
         </div>
       </div>
       <div className="modal-body-container">
-        <div style={{ maxWidth: "28em" }}>
-          <center>
-            <p className="memory-card-title text-shadow modal-body" style={{ fontSize: "32px", marginBottom: "8px" }}>Memory Card <span style={{ fontSize: "24px" }}> (PS2) / </span> {memoryCardName}</p>
-            <h1 className="text-shadow arial-lighter title" style={{ marginTop: "0", fontSize: "48px", marginBottom: "8px" }} >{title}</h1>
-            <p className="text-shadow modal-body" style={{ marginBottom: "0", marginTop: "0", fontSize: "24px", letterSpacing: "2px" }}>{date.start}&nbsp; — &nbsp;{date.end}</p>
-            <p className="text-shadow modal-body" style={{ marginTop: "0", marginBottom: "40px", fontSize: "24px", letterSpacing: "2px" }}>{memory}</p>
-          </center>
-          <p className="text-shadow-thinner modal-body" style={{ lineHeight: "1.5" }}>{summary}</p>
-          <i className="text-shadow-thinner modal-body" style={{ fontSize: "16px" }}>Tech Stack: {techStack}</i>
-          <ul>
-            {
-              bulletPoints.map((bullet, index) => {
-                return <li key={index} className="memory-card-bulletpoints text-shadow-thinner modal-body" style={{ lineHeight: "1.8" }}>{bullet}</li>
-              })
-            }
-          </ul>
-          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <p
-              href={link}
-              target="_blank"
-              rel="noopener"
-              className={`arial-lighter text-shadow-thinner selectable-text ${currHighlighted === Highlight.Link && link !== "" ? 'highlight' : 'not-highlighted'}`}
-              style={{ cursor: link === "" ? "not-allowed" : "pointer" }}
-              onMouseEnter={() => setCurrHighLighted(Highlight.Link)}
-            >Link</p>
-            <p
-              className={`arial-lighter text-shadow-thinner selectable-text ${currHighlighted === Highlight.Back ? 'highlight' : 'not-highlighted'}`}
-              onMouseEnter={() => setCurrHighLighted(Highlight.Back)}
-              onClick={() => fadeout()}
-            >
-              Back
-            </p>
-          </div>
+        <center>
+          <p className="memory-card-title text-shadow modal-body" style={{ fontSize: "32px", marginBottom: "8px" }}>Memory Card <span style={{ fontSize: "24px" }}> (PS2) / </span> {memoryCardName}</p>
+          <h1 className="text-shadow arial-lighter title" style={{ marginTop: "0", fontSize: "48px", marginBottom: "8px" }} >{title}</h1>
+          <p className="text-shadow modal-body" style={{ marginBottom: "0", marginTop: "0", fontSize: "24px", letterSpacing: "2px" }}>{date.start}&nbsp; — &nbsp;{date.end}</p>
+          <p className="text-shadow modal-body" style={{ marginTop: "0", marginBottom: "40px", fontSize: "24px", letterSpacing: "2px" }}>{memory}</p>
+        </center>
+        <p className="text-shadow-thinner modal-body" style={{ lineHeight: "1.5" }}>{summary}</p>
+        <i className="text-shadow-thinner modal-body" style={{ fontSize: "16px" }}>Tech Stack: {techStack}</i>
+        <ul>
+          {
+            bulletPoints.map((bullet, index) => {
+              return <li key={index} className="memory-card-bulletpoints text-shadow-thinner modal-body" style={{ lineHeight: "1.8" }}>{bullet}</li>
+            })
+          }
+        </ul>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "128px" }}>
+          <p
+            href={link}
+            target="_blank"
+            rel="noopener"
+            className={`arial-lighter text-shadow-thinner selectable-text ${currHighlighted === Highlight.Link && link !== "" ? 'highlight' : 'not-highlighted'}`}
+            style={{ cursor: link === "" ? "not-allowed" : "pointer" }}
+            onMouseEnter={() => setCurrHighLighted(Highlight.Link)}
+          >Link</p>
+          <p
+            className={`arial-lighter text-shadow-thinner selectable-text ${currHighlighted === Highlight.Back ? 'highlight' : 'not-highlighted'}`}
+            onMouseEnter={() => setCurrHighLighted(Highlight.Back)}
+            onClick={() => fadeout()}
+          >
+            Back
+          </p>
         </div>
       </div>
     </>
