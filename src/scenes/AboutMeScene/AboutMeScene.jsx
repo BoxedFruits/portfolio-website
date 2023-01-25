3c468c86e00fa1e3829f006f8eb18a59a4fe76f5
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-01-25 17:09:16 -0500

    scaffoling

--- src/scenes/AboutMeScene/AboutMeScene.jsx
+++ src/scenes/AboutMeScene/AboutMeScene.jsx
@@ -1,9 +1,19 @@
+import BackButton from "../../components/BackButton/BackButton";
+
 const AboutMeScene = ({ prevScene }) => {
 
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div>
+        <p style={{ color: "white", fontSize: "350px", marginTop: "28px", marginBottom: "34px" }}>
+          About Me
+        </p>
+        <p style={{ color: "white", fontSize: "150px", marginTop: "18px" }}>
+          Under Construction 🏗️
+        </p>
+      </div>
+      <BackButton onClick={prevScene} />
+    </>
   )
 }
 
