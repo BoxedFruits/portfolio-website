dceb1f438f0972d0b3d485145bc2ce3e0e1a1c67
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-08-22 20:58:22 -0400

    update resume link

--- src/scenes/AboutMeScene/AboutMeScene.jsx
+++ src/scenes/AboutMeScene/AboutMeScene.jsx
@@ -20,7 +20,7 @@ const Content = {
   },
   Resume: {
     header: "Resume",
-    content: "https://drive.google.com/file/d/1Q9f7btjdnFQzfXl39Lt3D4e3jA8o0wOi/view?usp=share_link"
+    content: "https://drive.google.com/file/d/1Yv-D2UZ53sU1NTJbD_BI84dwZ8_Z2jFT/view?usp=sharing"
   },
   ContactInfo: {
     header: "Contact Info",
